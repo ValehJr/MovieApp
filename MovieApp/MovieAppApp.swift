8434cc67dc2ef3b0ec92ee47437df44568fde2cd
ValehJr <i.valehjr@gmail.com>
2026-01-04 13:03:12 +0100

    Initial commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Valeh Ismayilov on 04.01.26.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
